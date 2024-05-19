1d352a2dc4aecb004c02830ae81ff48873c1c1ae
Tell Aulin <tell@tellemontage.se>
2024-05-19 14:45:37 +0000

    Test final result and connection with Google worksheet in renamed test_update_worksheet-py

---
+++ test_update_worksheet.py
@@ -0,0 +1,62 @@
+# Import
+import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+from colorama import Fore, init
+
+# Setup för Colorama
+init(autoreset=True)
+
+# Const
+EXTRA_SCORE = 200
+FULLY_WORD_SCORE = 500
+
+# Getting today's date
+date = datetime.datetime.today()
+Date = str(datetime.date.today())
+
+# Google Sheets API setup
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+# Authorization for Google Sheets API
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("ET_Hangman_Leaderboard")
+
+# Getting the leaderboard worksheet
+leaderboard = SHEET.worksheet("leaderboard")
+
+
+def update_worksheet(Name, Score, Country):
+    """
+    Update a new row in the Hangman worksheet.
+    This updates a new row with the rank, name, date, score, and country.
+    """
+    print(f"\t{Fore.GREEN}Updating Leaderboard...\n")
+
+    # Get all values in the worksheet
+    all_values = leaderboard.get_all_values()
+    # Determine the next rank
+    Rank = len(all_values)
+
+    # Append the new row with rank, name, date, score, and country
+    leaderboard.append_row([Rank, Name, Date, Score, Country])
+    print(f"\t{Fore.GREEN}Leaderboard Update successful.\n")
+
+
+# Testa funktionen med ett testfall
+def test_update_worksheet():
+    Name = "WORST PLAYER EVER"
+    Score = 0
+    Country = "NORWAY"
+    update_worksheet(Name, Score, Country)
+
+
+# Kör testet
+if __name__ == "__main__":
+    test_update_worksheet()
