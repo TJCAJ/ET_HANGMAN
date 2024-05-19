3804000ed056c64be3bdaa601ad3be469e09bc1b
Tell Aulin <tell@tellemontage.se>
2024-05-19 18:51:28 +0000

    Add the leaderboard worksheet to main game function

--- test_game_function.py
+++ test_game_function.py
@@ -39,3 +39,26 @@ A - PLAY AGAIN
 B - LEADERBOARD
 C - EXIT THE GAME
 """
+
+
+
+
+# Getting the leaderboard worksheet
+leaderboard = SHEET.worksheet("leaderboard")
+
+
+def update_worksheet(name, score, country):
+    """
+    Update a new row in the Hangman worksheet.
+    This updates a new row with the rank, name, date, score, and country.
+    """
+    print(f"\t{Fore.GREEN}Updating Leaderboard...\n")
+
+    # Get all values in the worksheet
+    all_values = leaderboard.get_all_values()
+    # Determine the next rank
+    rank = len(all_values)
+
+    # Append the new row with rank, name, date, score, and country
+    leaderboard.append_row([rank, name, date, score, country])
+    print(f"\t{Fore.GREEN}Leaderboard Update successful.\n")
