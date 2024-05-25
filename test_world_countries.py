56233adefc539b5eed802937db51b3d642548033
Tell Aulin <tell@tellemontage.se>
2024-05-25 09:09:56 +0000

    Create a test sheet to validate countries

---
+++ test_world_countries.py
@@ -0,0 +1,17 @@
+"""This file runs the ET Hangman game."""
+
+# Modules and libraries
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Google Sheets API setup
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+# Authorization for Google Sheets API
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
